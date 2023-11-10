324423bd762916912cc88bea0a63d921266d3c87
Du <du@localhost.localdomain>
2023-11-10 11:16:58 -0300

    geraçao do arquivo executavel

--- hello.c
+++ hello.c
@@ -2,9 +2,14 @@
 /*
 inblock (em bloco)
 */
+
+//incluindo a biblioteca de entrada e saida padrao(stdio)
 #include <stdio.h>
 
+// funcao main responsavel pela execuçao do arquivo
 int main(){
-    printf("Hello World!")
+    //printf exibe a mensagem na tela
+    printf("Hello World!\n");
+    //retorno para a funçao main q foi declarada com int
     return 0;
 }
