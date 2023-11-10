f6925b785bfaacd437195c9b03bc710c52554c7a
Du <mariaegsp@gmail.com>
2023-11-10 11:53:22 -0300

    criaçao do arquivo primeiro com variavel numerica

---
+++ primeiro.c
@@ -0,0 +1,11 @@
+//utilizaçao de variaveis
+// incluir a biblioteca stdio para entrada de dados
+#include <stdio.h>
+int main(){
+    //declaraçao da variavel valor c o tipo inteiro e numero 10
+    int valor = 10;
+    printf("Meu irmao tem %d anos de idade\n", valor);
+    
+    return 0;
+
+}
